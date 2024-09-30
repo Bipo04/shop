07d0aac1d0494583e7c50b7bbf1bbb5b845eb76b
Bipo04 <ledang1324@gmail.com>
2024-09-30 21:31:19 +0700

    add chatbot

--- mvc/views/block_client/footer.php
+++ mvc/views/block_client/footer.php
@@ -1,3 +1,19 @@
+<script>
+    (function(d,t) {
+    var BASE_URL="https://app.ahaspot.com";
+    var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+    g.src=BASE_URL+"/packs/js/sdk.js";
+    g.defer = true;
+    g.async = true;
+    s.parentNode.insertBefore(g,s);
+    g.onload=function(){
+        window.chatwootSDK.run({
+        websiteToken: 'grivjQe7GdHdxDUB6YCwgarS',
+        baseUrl: BASE_URL
+        })
+    }
+    })(document,"script");
+</script>
 <footer class="py-3 my-4">
     <ul class="nav justify-content-center border-bottom pb-3 mb-3">
         <li class="nav-item"><a href="#" class="nav-link text-body-secondary" style="font-size: 14px;">Home</a>
